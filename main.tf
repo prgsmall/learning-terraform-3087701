abf75f2b67e2d7952ecfd137b171a8ba22794c87
Peter R. Guerzenich-Small <prgsmall@gmail.com>
2023-07-05 17:29:59 -0400

    changed version of sg

--- main.tf
+++ main.tf
@@ -44,10 +44,10 @@ resource "aws_instance" "blog" {
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.0"
-  name    = "blog"
+  version = "4.13.0"
 
   vpc_id = module.blog_vpc.vpc_id
+  name   = "blog"
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
